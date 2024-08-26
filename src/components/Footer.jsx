dd7a62f9a0200afb7e71a08f8dc5199ea8bcec3d
Viola Bergere <viola.bergere@gmail.com>
2024-08-26 19:56:36 +0000

    feat: create FAQ page with responsive card layout
    - Add FAQPage component with a list of frequently asked questions.
    - Wrapp each question and answer in a responsive card.
    - Ensure the layout adapts to various screen sizes, with wider cards on large screens.
    - Update the styling to center content and adjust spacing for better readability.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
@@ -6,7 +7,7 @@ function Footer() {
             <div className="container mx-auto text-center">
                 <div className="mt-2">
                     <a href="/contact" className="text-customGreen hover:font-semibold mx-2 px-2">Contact Us</a>
-                    <a href="/faq" className="text-customGreen hover:font-semibold mx-2">FAQ</a>
+                    <Link to="/faq" className="text-customGreen hover:font-semibold mx-2">FAQ</Link>
                     <a href="/privacy-policy" className="text-customGreen hover:font-semibold mx-2">Privacy Policy</a>
                 </div>
                 <p className="text-sm pt-2">&copy;2024 ReBuild Ireland<br></br>All rights reserved</p>
--- src/components/OfficerCard.jsx
+++ src/components/OfficerCard.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 function OfficerCard({ officer }) {
     return (
-        <div className="w-80 max-w-xs rounded overflow-hidden shadow mx-auto p-3 m-3">
+        <div className="bg-primarylight w-80 max-w-xs rounded overflow-hidden shadow mx-auto p-3 m-3">
             <img className="w-full h-48 object-cover" src={officer.photo} alt={officer.name} />
             <div>
                 <h2 className="font-semibold uppercase m-2">{officer.name}</h2>
                 <p className="text-gray-700 text-base mb-1">{officer.county}</p>
                 {officer.email ? (
-                    <p className="text-gray-700 text-base mb-1">Email: {officer.email}</p>
+                    <p className="text-gray-700 text-sm mb-1">Email: {officer.email}</p>
                 ) : (
-                    <p className="text-gray-500 text-base mb-1">Email: Not Available</p>
+                    <p className="text-gray-500 text-sm mb-1">Email: Not Available</p>
                 )}
                 {officer.phone ? (
-                    <p className="text-gray-700 text-base">Phone: {officer.phone}</p>
+                    <p className="text-gray-700 text-sm">Phone: {officer.phone}</p>
                 ) : (
-                    <p className="text-gray-500 text-base">Phone: Not Available</p>
+                    <p className="text-gray-500 text-sm">Phone: Not Available</p>
                 )}
             </div>
         </div>
