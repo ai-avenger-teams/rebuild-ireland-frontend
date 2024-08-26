fd063dc52ed73eae1b232a016efd567f1a983816
Viola Bergere <viola.bergere@gmail.com>
2024-08-26 11:27:48 +0000

    feat: add Navbar component for site navigation, create Officers page
    - Create Navbar component with links to Home, Officers and Contact pages
    - Include Navbar component in App.jsx to display on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./App.css";
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Officers from './pages/Officers';
 import Contact from './pages/ContactPage';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/officers" element={<Officers />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Officers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Officers() {
+    return (
+        <div class="mx-auto">
+            <h1 class="text-3xl">Vacant property officers</h1>
+        </div>
+    );
+}
+
+export default Officers;
