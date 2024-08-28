6ed33b681894cb73e1c8fa128ac8c29f61f0b122
Viola Bergere <viola.bergere@gmail.com>
2024-08-28 12:57:22 +0000

    chore: Add images and descriptions to team page

--- src/components/TeamMemberCard.jsx
+++ src/components/TeamMemberCard.jsx
@@ -2,20 +2,22 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function TeamMemberCard({ member }) {
     return (
-        <div className="bg-primarylight w-80 max-w-xs rounded-lg overflow-hidden shadow mx-auto p-3 m-3">
-            <img className="w-full h-48 object-cover" src={member.photo} alt={member.name} />
-            <div>
-                <h2 className="font-semibold uppercase m-2">{member.name}</h2>
-                <p className="text-gray-700 text-base mb-2">{member.description}</p>
+        <div className="bg-primarylight w-80 max-w-xs rounded-lg overflow-hidden shadow-lg mx-auto p-3 m-3 flex flex-col justify-between">
+            <img className="w-full h-50 object-cover rounded-lg" src={member.photo} alt={member.name} />
+            <div className="flex-grow flex flex-col justify-between">
+                <div>
+                    <h2 className="font-semibold uppercase m-2">{member.name}</h2>
+                    <p className="text-gray-700 text-base mb-2">{member.description}</p>
+                </div>
                 <div className="flex justify-center space-x-3 mt-2">
                     {member.github && (
                         <a href={member.github} target="_blank" rel="noopener noreferrer" aria-label={`${member.name}'s GitHub`}>
-                            <FaGithub className="text-customLightGreen hover:text-customGreen text-2xl" />
+                            <FaGithub className="text-customLightGreen hover:text-customGreen text-2xl rounded-lg shadow-lg" />
                         </a>
                     )}
                     {member.linkedin && (
                         <a href={member.linkedin} target="_blank" rel="noopener noreferrer" aria-label={`${member.name}'s LinkedIn`}>
-                            <FaLinkedin className="text-customLightGreen hover:text-customGreen text-2xl" />
+                            <FaLinkedin className="text-customLightGreen hover:text-customGreen text-2xl rounded-lg shadow-lg" />
                         </a>
                     )}
                 </div>
--- src/pages/TeamMemberPage.jsx
+++ src/pages/TeamMemberPage.jsx
@@ -5,38 +5,38 @@ import TeamMemberCard from '../components/TeamMemberCard';
 const members = [
     {
         name: 'Viola Bergere',
-        photo: 'path/to/photo.jpg',
-        description: 'Full Stack Developer',
+        photo: 'src/assets/viola.JPEG',
+        description: 'Frontend Developer, UX/UI Designer, Idea Creator',
         github: 'https://github.com/violaberg',
         linkedin: 'https://www.linkedin.com/in/viola-bergere-5a668699/'
     },
     {
-        name: 'Darrach Barneveld',
-        photo: 'path/to/photo.jpg',
-        description: 'Full Stack Developer',
-        github: 'https://github.com/DarrachBarneveld',
-        linkedin: 'https://www.linkedin.com/in/darrach-barneveld-2b493511b/'
-    },
-    {
         name: 'Stephen Dawson',
-        photo: 'path/to/photo.jpg',
-        description: 'Full Stack Developer',
+        photo: 'src/assets/stephen.jpg',
+        description: 'Backend Developer, AI Product Developer',
         github: 'https://github.com/stephendawsondev',
         linkedin: 'https://linkedin.com/in/johndoe'
     },
     {
-        name: 'Jhoan Trujillo',
-        photo: 'path/to/photo.jpg',
-        description: 'Full Stack Developer',
-        github: 'https://github.com/jhoanTrujillo',
-        linkedin: 'https://www.linkedin.com/in/jhoan-trujillo-92b03517b/'
-    },
-    {
         name: 'Charles J. Fowler',
-        photo: 'path/to/photo.jpg',
-        description: 'Full Stack Developer',
+        photo: 'src/assets/cj.png',
+        description: 'AI Product Developer, Team Project Coordinator, DevOps & Cloud',
         github: 'https://github.com/iPoetDev',
         linkedin: 'https://www.linkedin.com/in/charlesjfowler/'
+    },
+    {
+        name: 'Darrach Barneveld',
+        photo: 'src/assets/darrach.jpg',
+        description: 'Full Stack Developer, Frontend Team Lead',
+        github: 'https://github.com/DarrachBarneveld',
+        linkedin: 'https://www.linkedin.com/in/darrach-barneveld-2b493511b/'
+    },
+    {
+        name: 'Jhoan Trujillo',
+        photo: 'src/assets/jhoan.png',
+        description: 'Full Stack Developer, AI Product Developer',
+        github: 'https://github.com/jhoanTrujillo',
+        linkedin: 'https://www.linkedin.com/in/jhoan-trujillo-92b03517b/'
     },
 ];
 
@@ -50,7 +50,7 @@ function Members() {
             </Helmet>
             <div className="mx-auto py-3">
                 <h1 className="text-customGreen font-semibold text-3xl py-3">ReBuild Ireland Team</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-3">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-3">
                     {members.map((member, index) => (
                         <TeamMemberCard key={index} member={member} />
                     ))}
